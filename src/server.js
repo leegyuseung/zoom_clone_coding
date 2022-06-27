5046d536ca9fd035a7f3aa8ad7f11ce86d8c3113
leegyuseung <lgs4002@naver.com>
2022-06-28 00:20:42 +0900

    express 설정 완료

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ import express, { application } from "express";
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 app.listen(3000);
